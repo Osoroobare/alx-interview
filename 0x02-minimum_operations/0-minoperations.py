247c9537f59ba2bc597c0d77e5f5b62589a12229
Osoroobare <brianosoro40@gmail.com>
2023-07-13 16:15:32 +0300

    minimum_operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-""" Module for fetching number of operations"""
+
+
+"""
+Calculates the minimum operations
+"""
 
 
 def minOperations(n):
-    """
-    minOperations
-    Fetches fewest number of operations needed to result in exactly n H characters
-    """
-    # all outputs should be at least two char:
-    if (n < 2):
-        return 0
-    x, y = 0, 2
-    while y <= n:
-        # if n evenly divides by y
-        if n % y == 0:
-        # total even-divisions by y = total operations
-            x += y
-        # set n to the remainder
-            n = n / y
-        # reduce y to find remaining smaller vals that evenly-divide n
-            y -= 1
-        # increment y until it evenly-divides n
-        y += 1
-    return x
+    answer = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            answer += x
+            n /= x
+        x += 1
+    return answer
