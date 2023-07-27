93f28877c559332ff5a2c600cc24216c10945d12
Osoroobare <brianosoro40@gmail.com>
2023-07-27 10:38:21 +0300

    utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,27 +3,26 @@
 UTF-8 Validation
 """
 
-
 def validUTF8(data):
     """
-    data: a list of integers
+    Data: This is a list of integers
     Return: True if data is a valid UTF-8
     encoding, else return False
     """
-    byte_count = 0
+    count = 0
 
-    for i in data:
-        if byte_count == 0:
-            if i >> 5 == 0b110 or i >> 5 == 0b1110:
-                byte_count = 1
-            elif i >> 4 == 0b1110:
-                byte_count = 2
-            elif i >> 3 == 0b11110:
-                byte_count = 3
-            elif i >> 7 == 0b1:
+    for j in data:
+        if count == 0:
+            if j >> 5 == 0b110 or j >> 5 == 0b1110:
+                count = 1
+            elif j >> 4 == 0b1110:
+                count = 2
+            elif j >> 3 == 0b11110:
+                count = 3
+            elif j >> 7 == 0b1:
                 return False
         else:
-            if i >> 6 != 0b10:
+            if j >> 6 != 0b10:
                 return False
-            byte_count -= 1
-    return byte_count == 0
+            count -= 1
+    return count == 0
