22d252291f2ca4c5eea32a87a992b115bfda7fca
Osoroobare <brianosoro40@gmail.com>
2023-08-10 22:04:38 +0300

    starwars_characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 /*script that prints all characters of a Star Wars movie:*/
 
+
 const request = require('request');
 const movieID = process.argv.slice(2);
 const endpoint = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
 
 function makeRequest (array, index) {
   if (index === array.length) {
