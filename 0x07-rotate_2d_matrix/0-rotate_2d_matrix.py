dabe1becb34420e9162c544379c38081dd4bc351
Osoroobare <brianosoro40@gmail.com>
2023-08-17 15:03:33 +0300

    rotate_2d_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-Rotate 2D Matrix
-"""
+""" Module for rotating a matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """rotate two dimension matrix 90 degrees clockwise
-    Args:
-        matrix (list[[list]]): a matrix
-    """
-    n = len(matrix)
-    for i in range(int(n / 2)):
-        y = (n - i - 1)
-        for j in range(i, y):
-            x = (n - 1 - j)
-            # current number
-            tmp = matrix[i][j]
-            # change top for left
-            matrix[i][j] = matrix[x][i]
-            # change left for bottom
-            matrix[x][i] = matrix[y][x]
-            # change bottom for right
-            matrix[y][x] = matrix[j][y]
-            # change right for top
-            matrix[j][y] = tmp
+
+
+   n = len(matrix)
+   for a in range(0, int(n / 2)):
+	# row=a
+        for b in range(a, n - a - 1):
+            # print((a,b))
+		#col=b
+            temp = matrix[a][b]
+
+            # print((a,b), (n - 1 - b,a))
+            matrix[a][b] = matrix[n - 1 - b][a]
+
+            # print((n - 1 - b,a), (n - 1 - a,n - 1 - b))
+            matrix[n - 1 - b][a] = matrix[n - 1 - a][n - 1 - b]
+
+            # print((n - 1 - a,n - 1 - b), (b,n - 1 - a))
+            matrix[n - 1 - a][n - 1 - b] = matrix[b][n - 1 - a]
+
+            # print((b,n - 1 - a))
+            matrix[b][n - 1 - a] = temp 
